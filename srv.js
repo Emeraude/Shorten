7fc8c87b7297791146e12d8bd9589e1a37494486
Emeraude <broggi_t@epitech.eu>
2014-10-19 15:08:49 +0200

    redirection works well, we juste have to add the links now

--- srv.js
+++ srv.js
@@ -2,6 +2,7 @@
 
 var app = require('express')();
 var mariadb = require('mariasql');
+var anybase = require('anybase');
 
 var db = new mariadb();
 db.connect({
@@ -13,20 +14,27 @@ db.connect({
 
 app.use(require('compression')())
     .use(require('serve-static')('public/'))
-    .engine('jade', require('jade').__express);
+    .engine('jade', require('jade').__express)
+    .listen(8080);
 
 var links = {};
+var shorted = {};
 
 db.query('SELECT * FROM Links')
     .on('result', function(res) {
 	res.on('row', function(row) {
 	    links[row.url] = row.id;
+	    shorted[row.id] = row.url;
 	});
     })
     .on('end', function() {
 	app.get('/:nb', function(req, res) {
-	    if (req.params.nb.match(/[\da-f]+/i))
-		;// redirection
+	    if (req.params.nb.match(/[\da-f]+/i)
+		&& shorted[anybase(10, req.params.nb.toUpperCase(), 16)]) {
+		res.status(302)
+		    .set('Location', shorted[anybase(10, req.params.nb.toUpperCase(), 16)])
+		    .end();
+	    }
 	    else
 		;// home page
 	})
