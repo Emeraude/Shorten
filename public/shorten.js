01d99adeb1f27bb388aa39d2cbbbc02e819cf629
Emeraude <broggi_t@epitech.eu>
2014-10-20 21:05:34 +0200

    a pretty spin is displayed

--- public/shorten.js
+++ public/shorten.js
@@ -2,13 +2,18 @@ $(function() {
     socket = io.connect(location.origin);
 
     $('form#short').submit(function(e) {
-	socket.emit('shorten-it', $(this).find('input[type=text]').val());
-	/* make the loading spin visible */
-	e.preventDefault();
+	url = $(this).find('input[type=text]').val();
+	socket.emit('shorten-it', url);
+	var spinner = new Spinner({
+	    top: '15px',
+	    left: '15px',
+	    corners: 1.0,
+	}).spin();
+	$('div#shortened').append($('<div data-url="'+url+'">').append(spinner.el));
     });
 
-    socket.on('shortened', function(shortened) {
-	console.log(shortened);
+    socket.on('shortened', function(url) {
+	console.log(url);
 	/* hide the loading spin */
     });
 });
